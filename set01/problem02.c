01c98830798d59bbd8da4115ec569530a790d505
preranabs <141308004+preranabs@users.noreply.github.com>
2024-01-22 21:20:43 +0530

    test02

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,9 +1,12 @@
 //2. Write a C program to add two numbers.
 #include <stdio.h>
 int main(){
-int a=4,b=5;
-int sum=0;
-sum=a+b;
-return sum;
-printf("the sum is:",sum);
+    int num1,num2;
+    printf("enter the first number: ");
+    scanf("%d",&num1);
+    printf("enter the second number: ");
+    scanf("%d",&num2);
+    int sum=num1+num2;
+    printf("sum:%d\n",sum);
+    return 0;
 }
