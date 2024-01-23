21a94439659e60837ca3ee5ec334261ddacee17d
preranabs <141308004+preranabs@users.noreply.github.com>
2024-01-23 10:25:23 +0530

    committed 1/23/2024, 10:25:22 AM

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,19 @@
 //2. Write a C program to add two numbers.
+#include <stdio.h>
 
+int main() {
+    
+    int num1, num2;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    int sum = num1 + num2;
+
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
