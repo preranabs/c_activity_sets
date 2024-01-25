9e4e0470425868a4f56ec73721084495debbbcee
preranabs <141308004+preranabs@users.noreply.github.com>
2024-01-25 10:21:32 +0530

    txt

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
-
 int input();
 int add(int a, int b);
 void output(int a, int b, int sum);
@@ -32,29 +30,4 @@ int add(int a, int b) {
 
 void output(int a, int b, int sum) {
     printf("Sum of %d and %d is: %d\n", a, b, sum);
-=======
-int input();
-int add(int a,int b);
-void output(int a,int b, int sum);
-
-int main(){
-    int num1, num2,result;
-    num1=input();
-    num2=input();
-    result = add(num1, num2);
-    output(num1,num2,result);
-    return 0;
-}
-int input(){
-    int num;
-    printf("Enter the number: ");
-    scanf("%d",&num);
-    return num;
-}
-int add(int a,int b){
-    return a+b;
-}
-void output(int a,int b, int sum){
-    printf("Sum of%d and %d is:%d\n",a,b,sum);
->>>>>>> f8f32cd79edff04b8c10891b6cd7a64ef01db0da
 }
