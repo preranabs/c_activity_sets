d5409084228b30f00549229c24f8ca00ccb252a5
preranabs <141308004+preranabs@users.noreply.github.com>
2024-02-20 21:10:59 +0530

    committed 2/20/2024, 9:10:58 PM

--- set02/problem01.c
+++ set02/problem01.c
@@ -6,11 +6,9 @@ void output(float base, float height, float area);
 
 int main() {
     float base, height, area;
-
     input(&base, &height);
     find_area(base, height, &area);
     output(base, height, area);
-
     return 0;
 }
 
