3f9020dfc9ed339d603c3328f53bb09c13136dc5
preranabs <141308004+preranabs@users.noreply.github.com>
2024-03-02 20:58:58 +0530

    committed 3/2/2024, 8:58:57 PM

--- set03/problem06.c
+++ set03/problem06.c
@@ -1,4 +1,6 @@
 // 6. Write a program to find the index of a substring of a string
+#include <stdio.h>
+#include <string.h>
 void input_string(char* a, char* b);
 int sub_str_index(char* string, char* substring);
-void output(char *string, char *substring, int index)
+void output(char *string, char *substring, int index);
