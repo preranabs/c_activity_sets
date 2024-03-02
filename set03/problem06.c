9df8f942a0716a217ed8d2e8e17f37e863e7698f
preranabs <141308004+preranabs@users.noreply.github.com>
2024-03-02 20:58:29 +0530

    committed 3/2/2024, 8:58:27 PM

--- set03/problem06.c
+++ set03/problem06.c
@@ -1,1 +1,4 @@
 // 6. Write a program to find the index of a substring of a string
+void input_string(char* a, char* b);
+int sub_str_index(char* string, char* substring);
+void output(char *string, char *substring, int index)
