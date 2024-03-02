1dde074482615ad3394978ac4edbfa312373eb16
preranabs <141308004+preranabs@users.noreply.github.com>
2024-03-02 20:59:28 +0530

    committed 3/2/2024, 8:59:27 PM

--- set03/problem06.c
+++ set03/problem06.c
@@ -4,3 +4,14 @@
 void input_string(char* a, char* b);
 int sub_str_index(char* string, char* substring);
 void output(char *string, char *substring, int index);
+
+int main() {
+    char main_string[100], sub_string[100];
+    input_string(main_string, sub_string);
+    int index = sub_str_index(main_string, sub_string);
+    
+    // Display the result
+    output(main_string, sub_string, index);
+
+    return 0;
+}
