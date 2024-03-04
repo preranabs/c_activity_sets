75941c27218440c9de2bc34c8982baa2faafb6e1
preranabs <141308004+preranabs@users.noreply.github.com>
2024-03-04 18:07:43 +0530

    committed 3/4/2024, 6:07:43 PM

--- set04/problem01.c
+++ set04/problem01.c
@@ -1,1 +1,27 @@
 // 1. Write a program to find sum of two fractions
+#include <stdio.h>
+
+int main() {
+    int num1, den1, num2, den2, res_num, res_den;    
+    input(&num1, &den1, &num2, &den2);
+    add(num1, den1, num2, den2, &res_num, &res_den);
+    output(num1, den1, num2, den2, res_num, res_den);
+    return 0;
+}
+
+void input(int *num1, int *den1, int *num2, int *den2) {
+    printf("Enter first fraction (numerator denominator): ");
+    scanf("%d %d", num1, den1);
+
+    printf("Enter second fraction (numerator denominator): ");
+    scanf("%d %d", num2, den2);
+}
+
+void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den) {
+    *res_num = (num1 * den2) + (num2 * den1);
+    *res_den = den1 * den2;
+}
+
+void output(int num1, int den1, int num2, int den2, int res_num, int res_den) {
+    printf("Sum of %d/%d and %d/%d is %d/%d\n", num1, den1, num2, den2, res_num, res_den);
+}
