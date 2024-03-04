3dbc29de094731a20da9e7949512ad097a46e604
preranabs <141308004+preranabs@users.noreply.github.com>
2024-03-04 18:29:50 +0530

    committed 3/4/2024, 6:29:49 PM

--- set04/problem03.c
+++ set04/problem03.c
@@ -1,1 +1,34 @@
-// 3. Write a program to find the `nCr` of given n and r
+#include <stdio.h>
+
+void input_n_and_r(int *n, int *r) {
+    printf("Enter the value of n: ");
+    scanf("%d", n);
+
+    printf("Enter the value of r: ");
+    scanf("%d", r);
+}
+
+int factorial(int num) {
+    if (num == 0)
+        return 1;
+    else
+        return num * factorial(num - 1);
+}
+
+int nCr(int n, int r) {
+    return factorial(n) / (factorial(r) * factorial(n - r));
+}
+
+void output(int n, int r, int result) {
+    printf("For n = %d and r = %d, nCr = %d\n", n, r, result);
+}
+
+int main() {
+    int n, r, result;
+    
+    input_n_and_r(&n, &r);
+    result = nCr(n, r);
+    output(n, r, result);
+    
+    return 0;
+}
