684929aad7965f376a525fd970e613f7f91b308b
preranabs <141308004+preranabs@users.noreply.github.com>
2024-03-05 18:52:01 +0530

    committed 3/5/2024, 6:52:01 PM

--- set05/problem01.c
+++ set05/problem01.c
@@ -1,1 +1,43 @@
 // 1.  Write a program to find the distance between two points.
+#include <stdio.h>
+#include <math.h>
+
+struct _point {
+    float x;
+    float y;
+};
+
+typedef struct _point Point;
+
+Point input() {
+    Point p;
+    printf("Enter x-coordinate: ");
+    scanf("%f", &p.x);
+    printf("Enter y-coordinate: ");
+    scanf("%f", &p.y);
+    return p;
+}
+
+void dist(Point a, Point b, float *res) {
+    *res = sqrt(pow((b.x - a.x), 2) + pow((b.y - a.y), 2));
+}
+
+void output(Point a, Point b, float res) {
+    printf("The distance between points (%.2f, %.2f) and (%.2f, %.2f) is %.2f\n", a.x, a.y, b.x, b.y, res);
+}
+
+int main() {
+    Point p1, p2;
+    float result;
+    
+    printf("Enter the first point:\n");
+    p1 = input();
+
+    printf("Enter the second point:\n");
+    p2 = input();
+
+    dist(p1, p2, &result);
+    output(p1, p2, result);
+
+    return 0;
+}
