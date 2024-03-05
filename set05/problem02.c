bf932317b13d5c586d2adadf733a846703436179
preranabs <141308004+preranabs@users.noreply.github.com>
2024-03-05 18:54:47 +0530

    committed 3/5/2024, 6:54:47 PM

--- set05/problem02.c
+++ set05/problem02.c
@@ -1,1 +1,36 @@
 // 2.  Write a program to find the weight of a camel, given height, length and stomach radius using four functions.
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159
+
+void input_camel_details(float *radius, float *height, float *length) {
+    printf("Enter stomach radius: ");
+    scanf("%f", radius);
+    printf("Enter height: ");
+    scanf("%f", height);
+    printf("Enter length: ");
+    scanf("%f", length);
+}
+
+float find_weight(float radius, float height, float length) {
+    return PI * pow(radius, 3) * sqrt(height * length);
+}
+
+void output(float radius, float height, float length, float weight) {
+    printf("Camel details:\n");
+    printf("Stomach radius: %.2f\n", radius);
+    printf("Height: %.2f\n", height);
+    printf("Length: %.2f\n", length);
+    printf("Weight: %.2f\n", weight);
+}
+
+int main() {
+    float radius, height, length, weight;
+
+    input_camel_details(&radius, &height, &length);
+    weight = find_weight(radius, height, length);
+    output(radius, height, length, weight);
+
+    return 0;
+}
